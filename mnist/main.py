356feae22b38f233e12b850d02223566967ee168
jeryfan <1295448630@qq.com>
2025-06-05 16:35:21 +0000

    mnist

--- mnist/main.py
+++ mnist/main.py
@@ -3,7 +3,7 @@ from testing import test_model
 import os
 
 if __name__ == '__main__':
-    # train_model(epochs=10)
+    train_model(epochs=10)
     model_path = os.listdir('models')[0]
     print(model_path)
     test_model(f"models/{model_path}")
